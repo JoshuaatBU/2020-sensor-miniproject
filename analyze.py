cba68b8e30812afac22940064a99efedd964b388
Joshua Taylor <joshuaat@bu.edu>
2020-09-13 18:43:07 -0400

    task 2 complete

--- analyze.py
+++ analyze.py
@@ -70,6 +70,13 @@ if __name__ == "__main__":
     temp_var = temps.var()
     temp_var_global = np.nanvar(temps)
     
+    print('The median temperatures of each room are (C): ')
+    print(temp_medians)
+    print('The global median temperature is: ' + str(temp_median_global) + '\n')
+    print('The variance of these temperatures are: ')
+    print(temp_var)
+    print('The global variance is: ' + str(temp_var_global) + '\n')
+
     
     #Calculate statistical details of the occupency
     occup = data['occupancy']
@@ -77,3 +84,94 @@ if __name__ == "__main__":
     occup_median_global = np.nanmedian(occup)
     occup_vars = occup.var()
     occup_var_global = np.nanvar(occup)
+    
+    print('The median occupancy of each room are: ')
+    print(occup_medians)
+    print('The global median occupancy is: ' + str(occup_median_global) + '\n')
+    print('The variance of these occupancies are: ')
+    print(occup_vars)
+    print('The global variance is: ' + str(occup_var_global) + '\n')
+    
+    #Model the distribution functions of the sensors - temperature
+    print('A histogram can provide a model of the probability distributions of the sensors \n')
+    print('For safety, the data has been filtered with a 5%-95% filter, because of some extreme outliers \n')
+    lab1_temps =  temps.lab1[temps.lab1 < temps.lab1.quantile(0.95)]
+    lab1_temps_prime = lab1_temps[lab1_temps > lab1_temps.quantile(0.05)]
+    
+    class_temps =  temps.class1[temps.class1 < temps.class1.quantile(0.95)]
+    class_temps_prime = class_temps[class_temps > class_temps.quantile(0.05)]
+    
+    office_temps =  temps.office[temps.office < temps.office.quantile(0.95)]
+    office_temps_prime = office_temps[office_temps > office_temps.quantile(0.05)]
+    
+    fig, axs = plt.subplots(1, 3, sharey=True, tight_layout=True)
+    axs[0].hist(lab1_temps_prime, bins = 30)
+    axs[0].set_xlabel('Lab 1 Temperature (C)')
+    axs[0].set_ylabel('Occurence')
+    axs[1].hist(class_temps_prime, bins=30)
+    axs[1].set_xlabel('Class 1 Temperature (C)')
+    axs[2].hist(office_temps_prime, bins=30)
+    axs[2].set_xlabel('Office Temperature (C)')
+    fig.show()
+    
+    
+    #Model the distribution functions of the sensors - Occupancy
+   
+    lab1_occup =  occup.lab1[occup.lab1 < occup.lab1.quantile(0.95)]
+    lab1_occup_prime = lab1_occup[lab1_occup > lab1_occup.quantile(0.05)]
+    
+    class_occup =  occup.class1[occup.class1 < occup.class1.quantile(0.95)]
+    class_occup_prime = class_occup[class_occup > class_occup.quantile(0.05)]
+    
+    office_occup =  occup.office[occup.office < occup.office.quantile(0.95)]
+    office_occup_prime = office_occup[office_occup > office_occup.quantile(0.05)]
+    
+    fig, axs = plt.subplots(1, 3, sharey=True, tight_layout=True)
+    axs[0].hist(lab1_occup_prime, bins = 5)
+    axs[0].set_xlabel('Lab 1 Occupancy')
+    axs[0].set_ylabel('Occurence')
+    axs[1].hist(class_occup_prime, bins=5)
+    axs[1].set_xlabel('Class 1 Occupancy')
+    axs[2].hist(office_occup_prime, bins=5)
+    axs[2].set_xlabel('Office Occupancy')
+    fig.show()
+    
+    
+    #Model the distribution functions of the sensors - CO2
+    co2 = data['co2']
+    lab1_co2 =  co2.lab1[co2.lab1 < co2.lab1.quantile(0.95)]
+    lab1_co2_prime = lab1_co2[lab1_co2 > lab1_co2.quantile(0.05)]
+    
+    class_co2 =  co2.class1[co2.class1 < co2.class1.quantile(0.95)]
+    class_co2_prime = class_co2[class_co2 > class_co2.quantile(0.05)]
+    
+    office_co2 =  co2.office[co2.office < co2.office.quantile(0.95)]
+    office_co2_prime = office_co2[office_co2 > office_co2.quantile(0.05)]
+    
+    fig, axs = plt.subplots(1, 3, sharey=True, tight_layout=True)
+    axs[0].hist(lab1_co2_prime, bins = 20)
+    axs[0].set_xlabel('Lab 1 CO2')
+    axs[0].set_ylabel('Occurence')
+    axs[1].hist(class_co2_prime, bins=20)
+    axs[1].set_xlabel('Class 1 CO2')
+    axs[2].hist(office_co2_prime, bins=20)
+    axs[2].set_xlabel('Office CO2')
+    fig.show()
+    
+    #Calculate the time series indices, and the distribution of them
+    time_difference = temps.index.to_series() - temps.index.to_series().shift()
+    time_difference = time_difference[1:len(time_difference)-1]
+    time_difference_mean = time_difference.dt.total_seconds().mean()
+    time_difference_var = time_difference.dt.total_seconds().var()
+    
+    print('The mean time in between arrivals is: ' + str(time_difference_mean) + '\n')
+    print('The variance in time between arrivals is: ' + str(time_difference_var) + '\n')
+    print('In stochastic processes, arrivals are often modeled as poisson distributions, yielding exponential waiting times between arrivals \n')
+    
+    print('Assuming a lambda of 1.078, an exponential distribution would have a mean of 0.927 and a variance of 0.860, which is the case here. There is some difference due to rounding in calculations \n')
+    plt.figure()
+    plt.hist(time_difference.dt.total_seconds(),bins = 30)
+    plt.ylabel('Occurences')
+    plt.xlabel('Seconds between arrivals')
+    plt.show()
+    print('From the source code, the distribution is generated from an erlang waiting time with k = 1, which simplifies to exponential as expected')
